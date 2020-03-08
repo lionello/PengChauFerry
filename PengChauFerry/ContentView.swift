0f535e66c480d6c59dd6116088271e344c9cb4a6
Lionello Lunesu <lio+git@lunesu.com>
2020-03-08 13:44:39 +0800

    Initial Commit

---
+++ PengChauFerry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PengChauFerry
+//
+//  Created by Lionello Lunesu on 8/3/2020.
+//  Copyright © 2020 Lionello Lunesu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
