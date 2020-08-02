732ab86259c61406120ac0cbd70280be81a3e3e5
Lionello Lunesu <lio+git@lunesu.com>
2020-08-02 18:59:23 +0800

    Drop unused index

--- Android/app/src/main/java/com/lunesu/pengchauferry/DbOpenHelper.kt
+++ Android/app/src/main/java/com/lunesu/pengchauferry/DbOpenHelper.kt
@@ -11,12 +11,11 @@ class DbOpenHelper(context: Context?) : SQLiteOpenHelper(context, if (context ==
         const val HOLIDAYS = "holidays"
 
         private const val DB_NAME = "ferry2"
-        private const val DB_VERSION = 10
+        private const val DB_VERSION = 11
 
         private const val CREATE_TABLE_TIMES =
             "CREATE TABLE $TIMES (time TEXT NOT NULL, `from` TEXT NOT NULL, `to` TEXT NOT NULL, durationMin INTEGER NOT NULL, days INTEGER NOT NULL, fare TEXT, via TEXT);"
-        private const val CREATE_INDEX_TIMES = "CREATE INDEX times_from_time ON $TIMES (`from`,time);"
-        private const val CREATE_UNIQUE_INDEX_TIMES = "CREATE UNIQUE INDEX times_unique ON $TIMES (time,`from`,`to`,days);"
+        private const val CREATE_UNIQUE_INDEX_TIMES = "CREATE UNIQUE INDEX times_unique ON $TIMES (`from`,`to`,time,days);"
 
         private const val CREATE_TABLE_HOLIDAYS =
             "CREATE TABLE $HOLIDAYS (date TEXT NOT NULL);"
@@ -25,7 +24,6 @@ class DbOpenHelper(context: Context?) : SQLiteOpenHelper(context, if (context ==
 
     override fun onCreate(db: SQLiteDatabase?) {
         db?.execSQL(CREATE_TABLE_TIMES)
-        db?.execSQL(CREATE_INDEX_TIMES)
         db?.execSQL(CREATE_UNIQUE_INDEX_TIMES)
         db?.execSQL(CREATE_TABLE_HOLIDAYS)
         db?.execSQL(CREATE_INDEX_HOLIDAYS)
@@ -38,10 +36,15 @@ class DbOpenHelper(context: Context?) : SQLiteOpenHelper(context, if (context ==
             6 -> db?.execSQL(CREATE_UNIQUE_INDEX_TIMES)
             7 -> {
                 db?.execSQL("DROP INDEX times_from;")
-                db?.execSQL(CREATE_INDEX_TIMES)
+                // db?.execSQL(CREATE_INDEX_TIMES) no longer used
             }
             8 -> db?.execSQL("ALTER TABLE $TIMES ADD COLUMN fare TEXT;")
             9 -> db?.execSQL("ALTER TABLE $TIMES ADD COLUMN via TEXT;")
+            10 -> {
+                db?.execSQL("DROP INDEX times_from_time;")
+                db?.execSQL("DROP INDEX times_unique;")
+                db?.execSQL(CREATE_UNIQUE_INDEX_TIMES)
+            }
         }
     }
 }
